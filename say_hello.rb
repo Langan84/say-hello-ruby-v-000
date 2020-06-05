74cf2a97fecefdd00bb30a4173eceec64522f69f
langan.kg Langan <langan.kg@gmail.com>
2020-06-05 10:07:18 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,4 @@
-# Build your say_hello method here
-def say_hello(name = "Ruby programer")
-  puts "Hello, #{name}"
+def say_hello(name = "Ruby Programmer!")
+  puts ("Hello, #{name}!")
 end
-
-say_hello
-
+say_hello()
